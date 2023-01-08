57ed8a26589ab9ff280f36974a500b45242fcaee
kha <ktran40@uncc.edu>
2023-01-08 15:19:45 -0500

    Create Todo entity

---
+++ backend/src/main/java/com/ktran/learningproject/todoapp/entities/Todo.java
@@ -0,0 +1,78 @@
+package com.ktran.learningproject.todoapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String description;
+    private Boolean done;
+
+    public Todo(Long id, String username, String description, Boolean done) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.done = done;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Todo todo)) return false;
+        return Objects.equals(id, todo.id) && Objects.equals(username, todo.username) && Objects.equals(description, todo.description) && Objects.equals(done, todo.done);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, description, done);
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                '}';
+    }
+}
