20d53d3f85444bdff5a9f03194e6fae46cff2dca
Kha Tran <khadieptran@gmail.com>
2023-02-10 16:55:01 -0500

    prepare to merge with main branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
-import './App.css'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Notes from './pages/Notes';
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Notes from "./pages/Notes";
 
 function App() {
+    const customTheme = createTheme({});
 
-const customTheme = createTheme({
-
-})
-
-  return (
-      <ThemeProvider theme={customTheme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Notes/>} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={customTheme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Notes />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
