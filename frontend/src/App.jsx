3698743d6e63cf32eea802d63461967c884d4f05
Kha Tran <khadieptran@gmail.com>
2023-02-10 16:44:52 -0500

    Add react router and created mockup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Button } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Notes from './pages/Notes';
+
 function App() {
 
 const customTheme = createTheme({
@@ -9,7 +11,11 @@ const customTheme = createTheme({
 
   return (
       <ThemeProvider theme={customTheme}>
-          <div>Hello</div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Notes/>} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
   );
 }
