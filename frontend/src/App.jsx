a5a2116977c97ea30d7f0870a07ed04469463bd4
Kha Tran <khadieptran@gmail.com>
2023-02-12 12:22:12 -0500

    Install daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <div className="text-3xl font-bold underline">
-      Hello World Kha!
-    </div>
-  )
+    return (
+        <div className="container mx-auto px-4">
+            Hello
+        </div>
+    );
 }
 
 export default App;
