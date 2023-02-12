37effc7b27bd4fc6ebdddf5807a337dc1caee0ae
HakNart <khadieptran@gmail.com>
2023-02-12 15:20:12 -0500

    setup notecard components to request data from db

---
+++ frontend/src/components/NoteCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NoteCard({note}) {
+  return (
+    <div>{note.title}</div>
+  )
+}
--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { retrieveAllNotes } from '../api/NoteApiService'
+import NoteCard from '../components/NoteCard';
 
 export default function Notes() {
+  const [notes, setNotes] = useState([]);
+  useEffect(()=>{
+    retrieveAllNotes()
+    .then(res => res.json())
+    .then(data => {
+      setNotes(data);
+    })
+  },[])
   return (
     <div>
-        Notes
+        {notes.map((note)=>(
+          <NoteCard note={note}/>
+        ))}
     </div>
   )
 }
